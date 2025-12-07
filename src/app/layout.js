5d372bae7558089cc8a7f9a818c920cddd5512fd
mohana nr <@gmail.com>
2025-12-07 15:00:39 -0800

    layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from 'next/font/google'
 import "./globals.css";
+import Layout from '@/component/Layout';
+import { ContextProdiver } from '@/context/ContextMenu';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -18,9 +17,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        {children}
+    <html lang="en" className={inter.className}>
+      <body className={inter.variable}>
+        <ContextProdiver>
+          <Layout>
+            {children}
+          </Layout>
+        </ContextProdiver>
       </body>
     </html>
   );
