2fb731ec42444274c03f8d5b6e5ae844dd1c5afc
mohana nr <@gmail.com>
2025-12-07 23:52:20 -0800

    Add initial Layout component setup

--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+"use client"
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '@/context/ContextMenu'
 import Navbar from './Navbar'
 import Slider from './Sidbar'
+import { usePathname, useRouter } from 'next/navigation'
 
-function Layout({children}) {
+function Layout({ children }) {
+  const { isLogin } = useContext(AppContext)
+  const router = useRouter()
+  const pathname = usePathname()
+  const [checked, setChecked] = useState(false) 
+
+  
+  useEffect(() => {
+    const loggedIn = localStorage.getItem("isLogin") === "true"
+    if (!loggedIn && pathname !== '/login') {
+      router.push('/login')
+    }
+    setChecked(true)  
+  }, [pathname, router])
+
+  if (!checked) return null
+  if (!isLogin && pathname !== '/login') return null
+
   return (
     <div>
       <Navbar />
-      <div>
+      <div className="flex">
         <Slider />
+        <main className="flex-1">{children}</main>
       </div>
-      {children}
     </div>
   )
 }
